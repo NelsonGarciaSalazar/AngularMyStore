dcacd10f01804d715648c7d9c4002add099f4005
Ceiba-NelsonGarcia <nelson.garcia@ceiba.com.co>
2022-04-11 09:38:21 -0500

    2 implementación formulario cart.service

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -8,13 +9,22 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent {
 
-  items = this.CartService.getItems();
+  items = this.cartService.getItems();
+
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
 
   constructor(
-    private CartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
   ) { }
 
-  ngOnInit(): void {
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
-
 }
